6a00dd70bae31224d4404f507917b3eaa221c605
aretty <aretty_company@naver.com>
2023-05-30 05:24:02 +0900

    modal add

--- src/public/js/common.js
+++ src/public/js/common.js
@@ -2,5 +2,12 @@ function setScreenSize() {
 	let vh = window.innerHeight * 0.01;
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
-setScreenSize();
-window.addEventListener('resize', setScreenSize);
+
+function openPop(id){
+	$("#"+id).addClass("on").addClass("animate__animated").addClass("animate__fadeInUpBig");
+}
+
+$(function(){
+	setScreenSize();
+	window.addEventListener('resize', setScreenSize);
+});
