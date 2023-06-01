0b15b0e96cf7c7717caf9eb7ea158d0d79443bec
aretty <dkdud4352@naver.com>
2023-06-01 14:36:58 +0900

    focus

--- src/public/js/common.js
+++ src/public/js/common.js
@@ -3,6 +3,10 @@ function setScreenSize() {
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
 
+const isMobile = () => {
+	return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+}
+
 function openPop(id){
 	$("#"+id).addClass("on").addClass("animate__animated").addClass("animate__fadeInUpBig");
 }
