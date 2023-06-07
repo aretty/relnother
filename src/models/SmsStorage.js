543c2d5cad6e9c68936df31bf090e78adacb67f5
aretty <dkdud4352@naver.com>
2023-06-07 19:05:38 +0900

    err modi

--- src/models/SmsStorage.js
+++ src/models/SmsStorage.js
@@ -7,10 +7,11 @@ class SmsStorage {
     static sendMessage(smsInfo){
         return new Promise((resolve,reject) => {
             let division = smsInfo.division;
+            let hp = smsInfo.hp;
 
             if(division == 'register'){
-                const existHpQuery = "select * from users where hp = ? order by idx desc limit 1";
-                db.query(existHpQuery, [smsInfo.hp], (err) => {
+                const existHpQuery = "select * from users where hp = ? order by idx desc limit 1;";
+                db.query(existHpQuery, [hp], (err) => {
                     if(err) reject(`${err}`);
 
                     if(data.length > 0){
@@ -20,10 +21,10 @@ class SmsStorage {
                         }
                         resolve(res);
                     } else {
-                        sendSms({ receivers: [smsInfo.hp], message: smsInfo.msg }).then((result) => {
+                        sendSms({ receivers: [hp], message: smsInfo.msg }).then((result) => {
                             if(parseInt(result.error_cnt) == 0){
                                 const query = "INSERT INTO sms_history(division, hp, msg, core, datetime) VALUES(?, ?, ?, ?, now());";
-                                db.query(query,[division, smsInfo.hp, smsInfo.msg, smsInfo.authNum], (err) => {
+                                db.query(query,[division, hp, smsInfo.msg, smsInfo.authNum], (err) => {
                                     if(err) reject(`${err}`);
                                     resolve({ success : true });
                                 });  
